766199fad89d69267023356ad80e056b11e12b0e
GVK9633 <gvijaykumarachary@gmail.com>
2026-02-06 16:08:04 +0530

    base module load.

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,35 @@
+from utils.model_loader import ModelLoader
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph, MessagesState, END, START
+from langgraph.prebuilt import ToolNode, tools_condition
+# from tools.weather_info_tool import WeatherInfoTool
+# from tools.place_search_tool import PlaceSearchTool
+# from tools.expense_calculator_tool import CalculatorTool
+# from tools.currency_conversion_tool import CurrencyConverterTool
+
+class GrapBuilder():
+    def __init__(self):
+        self.system_prompt = SYSTEM_PROMPT
+
+    def agent_function(self, state: MessagesState):
+        # Implementation for building a graph from the provided data
+        """Main agent function"""
+        user_question = state["messages"]
+        input_question = [self.system_prompt] + user_question
+        response = self.llm_with_tools.invoke(input_question)
+        return {"messages": [response]}
+    
+    def build_graph(self, data):
+        # Implementation for constructing the graph structure
+        graph_builder = StateGraph(MessagesState)
+        graph_builder.add_node("agent", self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools=self.tools))
+        graph_builder.add_edge(START,"agent")
+        graph_builder.add_conditional_edges("agent",tools_condition)
+        graph_builder.add_edge("tools","agent")
+        graph_builder.add_edge("agent",END)
+        self.graph = graph_builder.compile()
+        return self.graph
+    
+    def __call__(self):
+        return self.build_graph()
